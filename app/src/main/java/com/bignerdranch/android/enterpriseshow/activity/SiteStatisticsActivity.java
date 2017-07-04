6516006d16979814b05d3764a5782ff8749dfce9
wanglianghai <1540446221@qq.com>
2017-07-04 22:24:23 +0800

    首页的activity的启动

---
+++ app/src/main/java/com/bignerdranch/android/enterpriseshow/activity/SiteStatisticsActivity.java
@@ -0,0 +1,19 @@
+package com.bignerdranch.android.enterpriseshow.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bignerdranch.android.enterpriseshow.R;
+
+/**
+ * Created by Administrator on 2017/7/4/004.
+ */
+
+public class SiteStatisticsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_site_statistics);
+    }
+}
--- app/src/main/java/com/bignerdranch/android/enterpriseshow/fragment/HomeFrag.java
+++ app/src/main/java/com/bignerdranch/android/enterpriseshow/fragment/HomeFrag.java
@@ -1,20 +1,31 @@
 package com.bignerdranch.android.enterpriseshow.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bignerdranch.android.enterpriseshow.R;
+import com.bignerdranch.android.enterpriseshow.activity.MySiteActivity;
+import com.bignerdranch.android.enterpriseshow.activity.SiteDetailActivity;
+import com.bignerdranch.android.enterpriseshow.activity.SiteStatisticsActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2017/7/3/003.
  */
 
 public class HomeFrag extends Fragment {
+    private static final String TAG = "HomeFrag";
     public static HomeFrag fragment;
+    private AppCompatActivity activity;
 
     private View rootView;
 
@@ -25,13 +36,37 @@ public class HomeFrag extends Fragment {
         return fragment;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = (AppCompatActivity) getActivity();
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         if (rootView == null) {
             rootView = inflater.inflate(R.layout.frag_home, container, false);
+            ButterKnife.bind(this, rootView);
         }
 
         return rootView;
     }
+
+    @OnClick({R.id.check_detail, R.id.my_show, R.id.add_show_linear})
+    void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.check_detail:
+                Log.i(TAG, "onClick: " + "0");
+                startActivity(new Intent(activity, SiteStatisticsActivity.class));
+                break;
+            case R.id.my_show:
+                startActivity(new Intent(activity, MySiteActivity.class));
+                break;
+            case R.id.add_show_linear:
+                startActivity(new Intent(activity, SiteDetailActivity.class));
+                break;
+        }
+    }
+
 }
